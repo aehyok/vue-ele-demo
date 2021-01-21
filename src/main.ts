626cad8866af7f7d6b99a2c0cb3af5d134b06337
刘启明 <455043818@qq.com>
2021-01-21 11:14:35 +0800

    vue3 element plus 引入，并通过按钮测试

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
 
-createApp(App)
+const app = createApp(App);
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
+app
   .use(store)
   .use(router)
   .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button type="primary" @click="test">test</el-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -8,10 +9,17 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { ElButton } from "element-plus";
 
 @Options({
   components: {
-    HelloWorld
+    HelloWorld,
+    [ElButton.name]: ElButton
+  },
+  methods: {
+    test() {
+      alert(1111);
+    }
   }
 })
 export default class Home extends Vue {}
